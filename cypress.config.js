2bc4221dbccade04d4d2f10479e488e76489e23c
deepikarav <140826090+deepikarav@users.noreply.github.com>
2023-09-14 15:59:06 -0400

    Feature (#7)
    * Updated example.json caseworker info
    
    * test commit
    
    * Update Cypress version and dependencies
    
    * Added new tests
    
    * Updated flaxy tests and resolved issues with modules
    
    * Visit Details changes (#5)
    
    * Modified to ignore example.json
    
    * Modified to ignore example.json
    
    * Modified to ignore example.json
    
    * Modified to ignore example.json
    
    * Modified to ignore example.json
    
    * Modified to ignore example.json
    
    * Modified to ignore example.json
    
    * Modified to ignore example.json
    
    * Updated Credentials
    
    * Test Commit
    
    * Changes to visit details
    
    * Visit details page changes
    
    * Modified to add waits to troubleshoot
    
    * Modified Config
    
    * modified .gitignore file
    
    * modified .gitignore file
    
    ---------
    
    Co-authored-by: deepikarav <140826090+deepikarav@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: tonyircc <tony.mathew@cic.gc.ca>
    Co-authored-by: tonyircc <108882328+tonyircc@users.noreply.github.com>
    Co-authored-by: Deepika Ravikumar <deepikaravikumar@deepikas-mbp.home>

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,18 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 8000,
-  reporter: 'mochawesome',
+  reporter: "mochawesome",
   env: {
-    url: 'https://vac-crdv-stg.apps.cic.gc.ca/en/',
+    url: 'https://vac-crdv-dev.apps.cic.gc.ca/en/',
   },
-  projectId: 'tuhfw1',
+  projectId: "tuhfw1",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
-})
+});
--- cypress/e2e/contactInfo.spec.js
+++ cypress/e2e/contactInfo.spec.js
@@ -1,73 +1,152 @@
-import SignInPage from "../support/pageObjects/SignInPage.js"
-import TermsAndConditionsPage from "../support/pageObjects/TermsAndConditionsPage"
-import HomePage from "../support/pageObjects/HomePage.js"
-import ContactInformationPage from "../support/pageObjects/ContactInformationPage"
-import Navigation from "../support/pageObjects/Navigation.js"
+import SignInPage from "../support/pageObjects/SignInPage.js";
+import TermsAndConditionsPage from "../support/pageObjects/TermsAndConditionsPage";
+import HomePage from "../support/pageObjects/HomePage.js";
+import ContactInformationPage from "../support/pageObjects/ContactInformationPage";
+import Navigation from "../support/pageObjects/Navigation.js";
 
-describe('Test Suite', function()
-{
-    
-    const signInPage = new SignInPage()
-    const termsAndConditionsPage = new TermsAndConditionsPage()
-    const homePage = new HomePage()
-    const navigation = new Navigation()
-    const contactInformationPage = new ContactInformationPage()
+describe("Test Suite", function () {
+  const signInPage = new SignInPage();
+  const termsAndConditionsPage = new TermsAndConditionsPage();
+  const homePage = new HomePage();
+  const navigation = new Navigation();
+  const contactInformationPage = new ContactInformationPage();
 
-    beforeEach(function()
-    {
+  beforeEach(function () {
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+  });
 
-        cy.fixture('example').then(function(data)
-        {
-            this.data=data
-        })
-        
-    })
+  it("Contact Information Form Fill Test", function () {
+    cy.visit(Cypress.env("url") + "signin");
+    signInPage.getUsernameTBox().type(this.data.signInUserName);
+    signInPage.getPasswordTBox().type(this.data.signInPassword);
+    signInPage.getContinueButton().click().wait(2000);
+    signInPage.getVerificationCodeTBox().type(this.data.verificationCode);
+    signInPage.getSignInButton().click();
+    cy.url().should("include", "/termsconditions");
+    termsAndConditionsPage.getAcceptButton().click();
+    homePage.getHomePageTitle().should("have.text", this.data.homePageTitleEn);
+    cy.url().should("include", "/home");
+    cy.log(
+      "User successfully logged into VAC Portal and navigated to Home page"
+    );
+    homePage
+      .getNewApplicationLink()
+      .click()
+      .then(function () {
+        cy.wait(2000);
+        cy.url().should("include", "/client/general/personal-details");
+      });
+    navigation.getNavbarContactInfo().click();
+    cy.url().should("include", "/contact-information");
+    contactInformationPage
+      .getCountryDropDown()
+      .click()
+      .clear()
+      .wait(2000)
+      .type(this.data.country)
+      .then(function () {
+        contactInformationPage.getCountryDropDown().then(($text) => {
+          if ($text.text().includes("Canada")) {
+            contactInformationPage
+              .getProvinceTBox()
+              .click()
+              .clear()
+              .type(this.data.province)
+              .should("have.value", this.data.province);
+            contactInformationPage
+              .getPostalCodeTBox()
+              .clear()
+              .type(this.data.postalCode)
+              .should("have.value", this.data.canadaPostalCode);
+          }
+          if ($text.text().includes("United States")) {
+            contactInformationPage
+              .getStateTBox()
+              .click()
+              .clear()
+              .type(this.data.state)
+              .should("have.value", this.data.state);
+            contactInformationPage
+              .getPostalCodeTBox()
+              .clear()
+              .type(this.data.postalCode)
+              .should("have.value", this.data.usPostalCode);
+          } else {
+            contactInformationPage
+              .getDistrictTBox()
+              .clear()
+              .type(this.data.district)
+              .should("have.value", this.data.district);
+            contactInformationPage
+              .getPostalCodeTBox()
+              .clear()
+              .type(this.data.otherPostalCode)
+              .should("have.value", this.data.otherPostalCode);
+          }
+        });
+        contactInformationPage
+          .getApartmentUnitTBox()
+          .clear()
+          .type(this.data.aptUnit)
+          .should("have.value", this.data.aptUnit);
+        contactInformationPage
+          .getStreetNumberTBox()
+          .type(this.data.streetNumber);
+        contactInformationPage.getStreetNameTBox().type(this.data.streetName);
+        contactInformationPage
+          .getCityTownTBox()
+          .clear()
+          .type(this.data.cityTown)
+          .should("have.value", this.data.cityTown);
+      });
+    contactInformationPage.getMailingAddressRadio().click();
+    contactInformationPage
+      .getEmailAddressTBox()
+      .clear()
+      .type(this.data.emailAddress)
+      .should("have.value", this.data.emailAddress);
 
-    it('Contact Information Form Fill Test', function()
-    {
-        cy.visit(Cypress.env('url') + 'signin')
-        signInPage.getUsernameTBox().type(this.data.signInUserName)
-        signInPage.getPasswordTBox().type(this.data.signInPassword)
-        signInPage.getContinueButton().click()
-        signInPage.getVerificationCodeTBox().should('be.visible').wait(15000)
-        signInPage.getSignInButton().click()
-        cy.url().should('include', '/termsconditions')
-        termsAndConditionsPage.getAcceptButton().click()
-        homePage.getHomePageTitle().should('have.text', this.data.homePageTitleEn)
-        cy.url().should('include', '/home')
-        cy.log("User successfully logged into VAC Portal and navigated to Home page")
-        homePage.getNewApplicationLink().click().then (function()
-        {
-            cy.wait(5000)
-            cy.url().should('include', '/client/general/personal-details')
-        })
-        navigation.getNavbarContactInfo().click()
-        contactInformationPage.getHeader().should('have.text', this.data.contactInformationPageHeader )
-        contactInformationPage.getPOTBox().clear().type(this.data.poBox).should('have.value', this.data.poBox)
-        contactInformationPage.getApartmentUnitTBox().clear().type(this.data.aptUnit).should('have.value', this.data.aptUnit)
-        contactInformationPage.getAddressTBox().clear().type(this.data.address).should('have.value', this.data.address)
-        contactInformationPage.getCityTownTBox().clear().type(this.data.cityTown).should('have.value', this.data.cityTown)
-        contactInformationPage.getCountryDropDown().type(this.data.country)
-        contactInformationPage.getCountryDropDownInput().click()
-        contactInformationPage.getProvinceTBox().clear().type(this.data.province).should('have.value', this.data.province)
-        contactInformationPage.getPostalCodeTBox().clear().type(this.data.postalCode).should('have.value', this.data.postalCode)
-        contactInformationPage.getDistrictTBox().clear().type(this.data.district).should('have.value', this.data.district)
-        contactInformationPage.getMailingAddressRadio().click()
-        contactInformationPage.getEmailAddressTBox().clear().type(this.data.emailAddress).should('have.value', this.data.emailAddress)
-        contactInformationPage.getAddTelephoneButton().click()
-        contactInformationPage.getPrimaryNumberRadio().click()
-        contactInformationPage.getTelephoneCountry().click()
-        contactInformationPage.getTelephoneType().click()
-        contactInformationPage.getDialCodeTBox().clear().type(this.data.dialCode).should('have.value', this.data.dialCode)
-        contactInformationPage.getTelephoneNumberTBox().clear().type(this.data.telephoneNumber).should('have.value', this.data.telephoneNumber)
-        contactInformationPage.getExtensionTBox().clear().type(this.data.extension).should('have.value', this.data.extension)
-        contactInformationPage.getAddNumberButton().should('be.enabled').click().then (function()
-        {
-            contactInformationPage.getTelephoneFromGrid().should('have.text',this.data.telephoneNumber)
-            contactInformationPage.getSaveButton().should('be.enabled').click() 
-        })
-        cy.url().should('include','/passport')
-        cy.log("Contact Information Form successfully completed")
-    })
-
-})
+    contactInformationPage.getAddTelephoneButton().then(function () {
+      if (this.data.telephoneNumberExists) {
+        contactInformationPage.getAddTelephoneButton().click();
+        contactInformationPage.getTelephonePrimaryRadio().click();
+        contactInformationPage.getTelephoneType().click();
+        contactInformationPage
+          .getTelephoneCountryRadio()
+          .click()
+          .then(($text) => {
+            if ($text.text().includes("Canada/US")) {
+              contactInformationPage
+                .getTelephoneNumberTBox()
+                .clear()
+                .type(this.data.telephoneNumber)
+                .should("have.value", this.data.telephoneNumber);
+              contactInformationPage
+                .getExtensionTBox()
+                .clear()
+                .type(this.data.extension)
+                .should("have.value", this.data.extension);
+            } else {
+              contactInformationPage
+                .getDialCodeTBox()
+                .clear()
+                .type(this.data.dialCode)
+                .should("have.value", this.data.dialCode);
+              contactInformationPage
+                .getTelephoneNumberTBox()
+                .clear()
+                .type(this.data.telephoneNumber)
+                .should("have.value", this.data.telephoneNumber);
+            }
+          });
+        contactInformationPage.getSaveTelephoneButton().click();
+      }
+    });
+    contactInformationPage.getFaxNumberRadio().click();
+    contactInformationPage.getSaveAndContinueButton().click();
+    cy.url().should("include", "/passport");
+    cy.log("Contact Information Form successfully completed");
+  });
+});
